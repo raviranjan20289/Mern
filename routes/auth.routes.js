60376e715cea4bf649c847c345238ae70a5cee99
raviranjan202 <mailraviranjan28@gmail.com>
2024-01-10 01:47:44 +0530

    zod implemented for form validations

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,9 +4,12 @@ const router = express.Router();
 
 const authController = require("../controllers/auth.controllers");
 
-router.post("/register", authController.register);
+const signupSchema = require("../validators/auth.validators");
+
+const validate = require("../middleware/validators.middleware");
+
+router.post("/register", validate(signupSchema), authController.register);
 
 router.post("/login", authController.login);
-
 
 module.exports = router;
